5b8727f742692a0264b74cdf0a8bf4a325042ee4
Zack! <76066480+ZazaBogara@users.noreply.github.com>
2021-10-29 23:12:36 +0300

    Update Lab3.cpp
    delete #include <math.h>
    changed the names of variables

--- Lab3.cpp
+++ Lab3.cpp
@@ -1,34 +1,33 @@
 #include <stdio.h>
-#include <math.h>
 
 int main()
 {
-	int n=0;
-	printf("n: ");
-	scanf("%d", &n);
-	n--;
-	int array[n];
-	for (int i=0;i<=n;i++)
+	int sizeOfArray=0;
+	printf("Size of array?: ");
+	scanf("%d", &sizeOfArray);
+	sizeOfArray--;
+	int array[sizeOfArray];
+	for (int i=0;i<=sizeOfArray;i++)
 	{
 		scanf("%d", &array[i]);
 	}
-	int max_max=0;
-	int which_is_max=0;
-	for (int i = 0; i <= n; i++)
+	int max_Counter=0;
+	int max_element=0;
+	for (int i = 0; i <= sizeOfArray; i++)
 	{
-		int x = array[i];
+		int transitive_element = array[i];
 		int counter = 0;
 		for (int j = 0; j <= n; j++)
 		{
-			if (x == array[j])
+			if (transitive_element == array[j])
 				counter++;
 		}
 		if (counter > max_max)
 		{
-			max_max=counter;
-			which_is_max = x;
+			max_Counter=counter;
+			max_element = temp;
 		}
 	}
-	printf("Counter:%d \n, Which is max:%d", max_max, which_is_max);
+	printf("Counter:%d \n, Which is max:%d", max_Counter, max_element);
 	return 0;
 }
